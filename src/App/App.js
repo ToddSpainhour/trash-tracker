59884eadff3ead42f916cfc87df6ddc10c39a73e
Todd Spainhour <toddspainhour@gmail.com>
2020-06-20 13:44:39 -0500

    user able to login and logout

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
+import Auth from '../components/pages/Auth/Auth';
 import MyNavbar from '../components/shared/MyNavbar/MyNavbar';
 import Welcome from '../components/pages/Welcome/Welcome';
 import Challenge from '../components/pages/Challenge/Challenge';
@@ -9,14 +12,38 @@ import CreateNewTrash from '../components/pages/CreateNewTrash/CreateNewTrash';
 import UpdateTrash from '../components/pages/UpdateTrash/UpdateTrash';
 import RandomTrashFacts from '../components/pages/RandomTrashFacts/RandomTrashFacts';
 import Footer from '../components/shared/Footer/Footer';
+
 import './App.scss';
 
+import fbConnection from '../helpers/data/connection';
+
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authenticated: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authenticated: true });
+      } else {
+        this.setState({ authenticated: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
     return (
       <div className="App">
         <h2>INSIDE APP COMPONENT</h2>
         <MyNavbar />
+        <Auth />
         <Welcome />
         <Challenge />
         <LearnMore />

7372d569881e5f64468dd8d08301403f5dfd8457
Todd Spainhour <toddspainhour@gmail.com>
2020-06-28 22:29:08 -0500

    edit functional with dropdown options from firebase

--- src/components/pages/EditTrash/EditTrash.js
+++ src/components/pages/EditTrash/EditTrash.js
@@ -37,24 +37,6 @@ class EditTrash extends React.Component {
       .catch((err) => console.error('cannot get singleTrash to edit'));
   }
 
-  // componentDidMount() {
-  //   const editId = this.props.match.params.trashId;
-  //   trashData.getSingleTrash(editId)
-  //     .then((response) => {
-  //       const trash = response.data;
-  //       this.setState({
-  //         trashName: trash.trashName,
-  //         trashDescription: trash.trashDescription,
-  //         // materialId: trash.materialId,
-  //         selectedMaterial: trash.materialId,
-  //         isRecyclable: trash.isRecyclable,
-  //         didYouRecycle: trash.didYouRecycle,
-  //         dateAdded: trash.dateAdded,
-  //       });
-  //     })
-  //     .catch((err) => console.error('cannot get singleTrash to edit'));
-  // }
-
   nameChange = (e) => {
     e.preventDefault();
     this.setState({ trashName: e.target.value });
@@ -69,11 +51,6 @@ class EditTrash extends React.Component {
     e.preventDefault();
     this.setState({ selectedMaterial: e.target.value });
   }
-
-  // materialChange = (e) => {
-  //   e.preventDefault();
-  //   this.setState({ materialId: e.target.value });
-  // }
 
   recyclabaleChange = (e) => {
     this.setState({ isRecyclable: e.target.value });
@@ -162,27 +139,6 @@ class EditTrash extends React.Component {
               {dropDownOptions}
                 </select>
             </div>
-
-            {/* <div>
-              <p>What material is the made of?</p>
-                <select
-                  className="dropdown"
-                  id="userSelectedMaterial"
-                  value={materialId}
-                  onChange={this.materialChange}>
-
-                  <option hidden>Pick the Material</option>
-                  <option>Paper</option>
-                  <option>Plastic</option>
-                  <option>Cardboard</option>
-                  <option>Glass</option>
-                  <option>Metal</option>
-                  <option>Rubber</option>
-                  <option>Styrofoam</option>
-                  <option>Food</option>
-                  <option>Other</option>
-                </select>
-            </div> */}
 
           <div className="isRecyclableRadio">
             <h6>Is the item recyclable?</h6>

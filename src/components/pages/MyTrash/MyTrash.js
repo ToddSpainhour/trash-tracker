443469f39c9a76057646f3f688f4418e71342d0c
Todd Spainhour <toddspainhour@gmail.com>
2020-07-03 16:57:51 -0500

    trash cards sorted from newest to oldest

--- src/components/pages/MyTrash/MyTrash.js
+++ src/components/pages/MyTrash/MyTrash.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import moment from 'moment';
 
 import authData from '../../../helpers/data/authData';
 import trashData from '../../../helpers/data/trashData';
@@ -18,15 +19,30 @@ class MyTrash extends React.Component {
     this.getTrash();
   }
 
+  // getTrash = () => {
+  //   const uid = authData.getUid();
+  //   trashData.getTrashByUid(uid)
+  //     .then((trashArray) => {
+  //       this.setState({ trashArray });
+  //       const newArray = this.state.trashArray.map((item) => item.didYouRecycle);
+  //       const amountOfRecycledItems = newArray.filter((a) => a === 'true');
+  //       const recycledNumber = amountOfRecycledItems.length;
+  //       console.error('recycledNumber is...', recycledNumber);
+  //       this.setState({ numberOfRecycledItems: recycledNumber });
+  //     })
+  //     .catch((err) => console.error('connot get trash...', err));
+  // }
+
   getTrash = () => {
     const uid = authData.getUid();
     trashData.getTrashByUid(uid)
       .then((trashArray) => {
+        const Moment = require('moment');
+        trashArray.sort((oldest, newest) => new Moment(newest.dateAdded).format('MMDDYYYY') - new Moment(oldest.dateAdded).format('MMDDYYYY'));
         this.setState({ trashArray });
         const newArray = this.state.trashArray.map((item) => item.didYouRecycle);
         const amountOfRecycledItems = newArray.filter((a) => a === 'true');
         const recycledNumber = amountOfRecycledItems.length;
-        console.error('recycledNumber is...', recycledNumber);
         this.setState({ numberOfRecycledItems: recycledNumber });
       })
       .catch((err) => console.error('connot get trash...', err));

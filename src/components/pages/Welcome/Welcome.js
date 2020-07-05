4f6ff21faa43ec3fa105d2259e7eb1bc6c059ae2
Todd Spainhour <toddspainhour@gmail.com>
2020-07-05 17:48:10 -0500

    added styling to welcome component

--- src/components/pages/Welcome/Welcome.js
+++ src/components/pages/Welcome/Welcome.js
@@ -10,7 +10,11 @@ class Welcome extends React.Component {
   render() {
     return (
       <div className="Welcome col-12">
-        <h5>Welcome component</h5>
+        <div className="header-area col-md-6 offset-md-3 col-sm-12 offset-sm-0">
+          <h6 className="header-intro">Welcome to</h6>
+          <h1 className="header">Trash Tracker</h1>
+            <h6 className="tagline">Learn More about the Things We Choose to Forget</h6>
+        </div>
         <Auth />
         <ChallengeTeaser />
         <LearnMoreTeaser />

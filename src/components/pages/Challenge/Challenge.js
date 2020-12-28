22f155866425bf16bdcecba0ab980345000f6dd2
Todd Spainhour <toddspainhour@gmail.com>
2020-12-28 11:25:32 -0600

    basic 7 day challenge layout and content complete

--- src/components/pages/Challenge/Challenge.js
+++ src/components/pages/Challenge/Challenge.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
+import recycleGraphic from '../../../images/recycle-logo.svg';
+import trashTrackerCards from '../../../images/trash-tracker-cards.JPG';
 import './Challenge.scss';
 
 class Challenge extends React.Component {
   render() {
     return (
       <div className="Challenge col-12">
-        <h3>Coming Soon: 7 Day Challenge Page</h3>
-        <p>Click 'Trash Tracker in the navbar to get back...</p>
+          <h1 className="page-header">Up For A Challenge?</h1>
+          {/* <p>Don't worry! This challenge is easy. All you need to do is track what you throw away or recycle for seven days.
+            This is a great peek into just how must we throw in the trash can.
+          </p> */}
+          <div>
+            <h3 className="section-title">Track Your Trash for Seven Days</h3>
+            <p>How much trash do you produce in a week? With Trash Tracker it's easy to find out. All you need to do is log in with your Google account,
+              and start adding items that are easy to forget.
+              Trash Tracker really shines when you look back at the items you previously added by putting
+              perspective just how must we throw away. If all that trash just comes from me in a week, how much do I produce in a year?
+              What about everyone in the city? The state? The country?</p>
+            <img src= { trashTrackerCards } className="trash-tracker-cards-img" alt="tracked trash cards"></img>
+          </div>
+
+          <div>
+            <h3 className="section-title">Take the Seven Day Trash Tracker Challenge</h3>
+            <img src= { recycleGraphic } alt="recycle logo"></img>
+            <p>Are you ready for the chellenge? Get started by logging in below!</p>
+          </div>
+
       </div>
     );
   }
